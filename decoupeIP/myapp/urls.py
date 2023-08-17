d5e62c74f40dd14ade85e50375b951b27933c06a
Onintsoa-Manjato <manjatoony@gmail.com>
2023-08-17 14:39:23 +0300

    done

--- decoupeIP/myapp/urls.py
+++ decoupeIP/myapp/urls.py
@@ -3,7 +3,9 @@ from . import views
 from .views import graph_view
 
 urlpatterns = [
-    path('', views.myapp, name='myapp'),
+    path('signup/', views.signup, name='signup'),
+    path('', views.log_in, name='login'), 
+    path('logout/', views.log_out, name='logout'),
     path('departement/', views.departement, name='departement'),
     path('departementlist/', views.departementlist, name='departementlist'),
     path('departement/<id>', views.detail_departement, name="detail_departement"),
@@ -15,5 +17,5 @@ urlpatterns = [
     path('sousreseau/<id>/update/', views.update_sousreseau, name="update_sousreseau"),
     path('sousreseau/<id>/delete/', views.delete_sousreseau, name="delete_sousreseau"),
     path('calculateur/', views.calculateur, name='calculateur'),
-    path('graph/', graph_view, name='graph')
+    path('graph/', graph_view, name='graph'),  
 ]
--- decoupeIP/myapp/views.py
+++ decoupeIP/myapp/views.py
@@ -1,29 +1,60 @@
-from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Departement, SousReseau
 from .forms import DepartementForm, sousReseauForm
-import ipaddress
 from .graph import get_graph
 from .caclulette import calc
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
-def myapp(request):
-    template = loader.get_template('main.html')
-    return HttpResponse(template.render())
+def signup(request):
+    context = {}
+    if request.method == 'POST':
+        form4 = UserCreationForm(request.POST)
+        if form4.is_valid():
+            user = form4.save()
+            login(request, user)
+            return redirect('login')
+    else:
+        form4 = UserCreationForm()
+    context["form4"] = form4
+    return render(request, 'signup.html', context)
+
+def log_in(request):
+    context = {}
+    if request.method == "POST":
+        form5 = AuthenticationForm(data=request.POST)
+        if form5.is_valid():
+            user = form5.get_user()
+            login(request,user)
+            return redirect('departementlist')
+    else:
+        form5 = AuthenticationForm()
+    context["form5"] = form5
+    return render(request,'login.html', context)
+
+def log_out(request):
+    logout(request)
+    return redirect('login')
 
 def departement(request):
+    if not request.user.is_authenticated:
+        return log_in(request)
     context = {}
 
     form = DepartementForm(request.POST or None)
     if form.is_valid():
         form.save()
-    
+        return redirect('departementlist')
     context['form'] = form
 
     return render(request, "departement.html", context)
 
 def departementlist(request):
+    if not request.user.is_authenticated:
+        return log_in(request)
     context = {}
 
     context["dataset"] = Departement.objects.all()
@@ -31,6 +62,8 @@ def departementlist(request):
     return render(request, "departementlist.html", context)
 
 def detail_departement(request, id):
+    if not request.user.is_authenticated:
+        return log_in(request)
     context = {}
 
     context["data"] = Departement.objects.get(id = id)
@@ -38,6 +71,8 @@ def detail_departement(request, id):
     return render(request, "detail_departement.html", context)
 
 def update_departement(request, id):
+    if not request.user.is_authenticated:
+        return log_in(request)
     context = {}
 
     obj = get_object_or_404(Departement, id = id)
@@ -49,6 +84,8 @@ def update_departement(request, id):
     return render(request, "update_departement.html", context)
 
 def delete_departement(request, id):
+    if not request.user.is_authenticated:
+        return log_in(request)
     context = {}
 
     obj = get_object_or_404(Departement, id = id)
@@ -61,6 +98,8 @@ def delete_departement(request, id):
     return render(request, "delete_departement.html", context)
 
 def sous_reseau(request):
+    if not request.user.is_authenticated:
+        return log_in(request)
     context = {}
 
     form2 = sousReseauForm(request.POST or None)
@@ -72,6 +111,8 @@ def sous_reseau(request):
     return render(request, "sousreseau.html", context)
 
 def sousreseaulist(request):
+    if not request.user.is_authenticated:
+        return log_in(request)
     context = {}
 
     context["dataset2"] = SousReseau.objects.all()
@@ -79,6 +120,8 @@ def sousreseaulist(request):
     return render(request, "sousreseaulist.html", context)
 
 def detail_sousreseau(request, id):
+    if not request.user.is_authenticated:
+        return log_in(request)
     context = {}
 
     context["data1"] = SousReseau.objects.get(id = id)
@@ -86,6 +129,8 @@ def detail_sousreseau(request, id):
     return render(request, "detail_sousreseau.html", context)
 
 def update_sousreseau(request, id):
+    if not request.user.is_authenticated:
+        return log_in(request)
     context = {}
 
     obj1 = get_object_or_404(SousReseau, id = id)
@@ -97,6 +142,8 @@ def update_sousreseau(request, id):
     return render(request, "update_sousreseau.html", context)
 
 def delete_sousreseau(request, id):
+    if not request.user.is_authenticated:
+        return log_in(request)
     context = {}
 
     obj2 = get_object_or_404(SousReseau, id = id)
@@ -109,6 +156,8 @@ def delete_sousreseau(request, id):
     return render(request, "delete_sousreseau.html", context)
 
 def calculateur(request):
+    if not request.user.is_authenticated:
+        return log_in(request)
     context = {}
     listRes = calc()
     
@@ -117,6 +166,8 @@ def calculateur(request):
     return render(request, "calculateur.html", context)
 
 def graph_view(request):
+    if not request.user.is_authenticated:
+        return log_in(request)
     # Get the graph
     graph = get_graph()
 
